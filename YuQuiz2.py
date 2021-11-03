c122677984a2bfb432b474a70b5212ffa49b677e
jonathanyu2000 <jonathanyu2000@gmail.com>
2021-11-02 20:23:04 -0400

    Update

--- YuQuiz2.py
+++ YuQuiz2.py
@@ -10,7 +10,7 @@ apikey='TpDU0UFqxP5VJOgKDj9TpaC18LshL0bN6nIs96Pm'
 
 url = "https://yfapi.net/v6/finance/quote"
 stocksymbol = input("Please type stock symbol: ")
-querystring = {"symbols":stocksymbol}
+querystring = {"symbols": stocksymbol}
 headers = {
   'x-api-key': apikey
    }
@@ -30,3 +30,5 @@ if response.status_code != 204:
           "Price:$" + str(stock_json['quoteResponse']['result'][0]["regularMarketPrice"]) + "\n"
           "Market Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime((stock_json['quoteResponse']['result'][0]["regularMarketTime"])))
 ))
+else:
+    stocksymbol = input("Please enter a valid Stock Ticker: ")
